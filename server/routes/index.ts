7dfd07a23b26a130329540686a54c6f90a2aa5f4
Thomas Coldwell <31568400+thomas-coldwell@users.noreply.github.com>
2024-10-29 21:35:11 +0000

    fix: Move them back once again
    see - https://strapi.io/blog/how-to-create-a-strapi-v4-plugin-server-customization-4-6

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -8,7 +8,29 @@ const routes = {
   },
   'content-api': {
     type: 'content-api',
-    routes: clientRoutes,
+    routes: [
+      ...clientRoutes,
+      {
+        method: 'POST',
+        path: '/mux-video-uploader/mux-assets',
+        handler: 'mux-asset.createBulk',
+        config: {
+          description: 'Create multiple MuxAssets',
+          policies: [],
+          prefix: false,
+        },
+      },
+      {
+        method: 'POST',
+        path: '/mux-video-uploader/submitRemoteUpload',
+        handler: 'mux.submitRemoteUpload',
+        config: {
+          description: 'Submit a remote video for upload',
+          policies: [],
+          prefix: false,
+        },
+      },
+    ],
   },
 };
 
